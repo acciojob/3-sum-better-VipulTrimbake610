a81d3d3a5818bccebc82a115678d60d8a50250c4
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-19 01:54:56 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-arr = [-1,2,1,-4];
 
 function threeSum(arr, target) {
-// write your code here
-  
+	// write your code here
+	
 }
 
+arr = [-1,2,1,-4];
+target = 1;
+threeSum(arr,target);
 module.exports = threeSum;
